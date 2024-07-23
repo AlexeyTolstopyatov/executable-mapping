a04c37dbf988967a6c096ce421cf8dee81a9f7d5
Шантажная Пена <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2024-07-24 02:25:52 +0700

    Update retyping.h
    fix codepage

--- src/retyping.h
+++ src/retyping.h
@@ -1,11 +1,5 @@
 #pragma once
 
-/*		(C) Толстопятов Алексей
- * Удивительно, но я очень устал писать инициализацию беззнаковых типов данных
- * В C# было куда удобнее работать с этим. Здесь я впервый раз...
- * 
- */
-
 typedef unsigned long	 __uint64_t;	/* DWORD */
 typedef unsigned char	 __uint8_t;		/* BYTE */
 typedef unsigned short	 __uint16_t;	/* WORD */

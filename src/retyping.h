ee24a1a3ef4214f7f2736c75b48703198deb0d29
Шантажная Пена <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2024-07-24 02:26:22 +0700

    Update retyping.h

--- src/retyping.h
+++ src/retyping.h
@@ -1,6 +1,6 @@
 #pragma once
 
-typedef unsigned long	 __uint64_t;	/* DWORD */
-typedef unsigned char	 __uint8_t;		/* BYTE */
-typedef unsigned short	 __uint16_t;	/* WORD */
-typedef unsigned int	 __uint32_t;	/* DWORD32 */
+typedef unsigned long	 __uint64_t;  /* DWORD */
+typedef unsigned char	 __uint8_t;	  /* BYTE */
+typedef unsigned short __uint16_t;  /* WORD */
+typedef unsigned int	 __uint32_t;  /* DWORD32 */
